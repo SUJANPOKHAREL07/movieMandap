2c93f6ee543c8fda1d11ab17ea7e441b529da845
sujan07 <sujanpokharel704@gmail.com>
2025-10-11 22:56:05 +0545

    backend setup with frontend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -25,7 +25,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: 'https://studio.apollographql.com',
+    origin: ['https://studio.apollographql.com', 'http://localhost:3000'],
     credentials: true,
   })
 );
